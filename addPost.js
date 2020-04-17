e658c2c3fdbdcb20ed8bcbfc79e467571661e571
LeonardoGuandalini <leoguanda@gmail.com>
2020-04-17 14:35:12 -0300

    Add files via upload

--- addPost.js
+++ addPost.js
@@ -1,22 +1,3 @@
-/* var textBox = document.querySelector("textarea");
-var post = document.querySelector('#write');
-
-function addPost(){ 
-	var text = textBox.value;
-    console.log(text);
-    var spaceDiv = document.createElement("div");
-    var spaceH2 = document.createElement("h2");
-    var spaceText = document.createTextNode(text);
-    spaceDiv.appendChild(spaceH2);
-    spaceH2.appendChild(spaceText);
-    spaceDiv.classList.add("post");
-    post.appendChild(spaceDiv);
-    var lineBreak = document.createElement("br");
-    post.appendChild(lineBreak);
- }
-
-
- */
 
  /* 
 1° - Variáveis necessárias: o div posts, o texto da textarea
@@ -26,7 +7,8 @@ criar um h2; colocar o h2 dentro deste div;
 
  */
 
-
+ /*
+var id = 1;
 
  function addPost() {
 
@@ -34,24 +16,91 @@ criar um h2; colocar o h2 dentro deste div;
     var profilePic = document.createElement("img");
     profilePic.src = "./comfy.jpg"
     profilePic.classList.add("profile-pic");
+
+    var deleteButton = document.createElement("span");
+    deleteButton.textContent = "Delete";
     var postSpace = document.getElementById('post-space');
     var textArea = document.querySelector("textarea");
     var text = textArea.value;
     
-
+    var textDiv = document.createElement("div");
+    textDiv.classList.add("post-text");
     var postDiv = document.createElement("div");
     postDiv.classList.add("post");
+    postDiv.setAttribute("id", id);
+    id += 1;
     postSpace.appendChild(postDiv);
+    postDiv.appendChild(textDiv);
 
     var textH2 = document.createElement("h2");
     var textNode = document.createTextNode(text);
     textH2.appendChild(textNode);
     postDiv.appendChild(profilePic);
-    postDiv.appendChild(textH2);
-
+    textDiv.appendChild(textH2);
+    textDiv.appendChild(deleteButton);  
+    deleteButton.onclick = removePost(id);
     postDiv.appendChild(profilePic);
     var lineBreak = document.createElement("br");
     postSpace.appendChild(lineBreak);
 
 
+ }
+
+ */
+
+ var id = 0;
+ var strId = String(id);
+ 
+ class Post {
+   constructor () {
+      var objId = id;
+      var profilePic = document.createElement("img");
+      profilePic.src = "./comfy.jpg"
+      profilePic.classList.add("profile-pic");
+  
+      var deleteButton = document.createElement("span");
+      deleteButton.textContent = "Delete";
+
+      var postSpace = document.getElementById('post-space');
+      var textArea = document.querySelector("textarea");
+      var text = textArea.value;
+      
+      var textDiv = document.createElement("div");
+      textDiv.classList.add("post-text");
+      var postDiv = document.createElement("div");
+      postDiv.classList.add("post");
+      postDiv.setAttribute("id", id);
+      id += 1;
+      postSpace.appendChild(postDiv);
+      postDiv.appendChild(textDiv);
+  
+      var textH2 = document.createElement("h2");
+      var textNode = document.createTextNode(text);
+      textH2.appendChild(textNode);
+      postDiv.appendChild(profilePic);
+      textDiv.appendChild(textH2);
+      textDiv.appendChild(deleteButton);
+      postDiv.appendChild(profilePic);
+      var lineBreak = document.createElement("br");
+      postSpace.appendChild(lineBreak);
+
+      deleteButton.addEventListener('dblclick', function() {
+         window.setTimeout(function() {
+            document.getElementById(objId).classList.add("invisible");
+
+      
+
+         }, 200);
+         
+   })
+
+   }
+   
+   
+
+
+      
+  
+   
+  
  }
